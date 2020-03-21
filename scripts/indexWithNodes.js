b7ede420a2b8ed289cd70948c95a1752ffed0158
ross-u <cirkovic.uros.srb@gmail.com>
2020-03-21 16:02:01 +0100

    Update starter code files

--- scripts/indexWithNodes.js
+++ scripts/indexWithNodes.js
@@ -17,7 +17,7 @@ var chat = document.getElementById("chat");
 var messageInput = document.getElementById("message-input");
 var sendButton = document.getElementById("message-send-btn-img");
 
-// EVENT CALLBACK FUNCTIONS
+// EHELPER FUNCTIONS
 function sendMessage() {
   var messageText = messageInput.value;
 
@@ -39,21 +39,21 @@ function displayMessage(message, sender) {
   var messageContent = document.createElement("p");
   var messageAvatar = document.createElement("img");
 
-  // CREATE TEXT NODES FOR NEW ELEMENTS
+  // CREATE TEXT NODES FOR THE NEW ELEMENTS
   var authorText = document.createTextNode(sender);
   var contentText = document.createTextNode(message);
 
-  // APPEND TEXT NODES TO THE ELEMENTS
+  // APPEND TEXT NODES TO THE CREATED ELEMENTS
   messageAuthor.appendChild(authorText);
   messageContent.appendChild(contentText);
 
-  // ADD IMAGE SRC TO THE IMAGE TAG
+  // ADD SRC ATTRIBUTE TO THE IMAGE TAG
   messageAvatar.setAttribute("src", "./images/ironhack.png");
 
   // APPEND TEXT ELEMENTS TO THE DIV
   newMessage.append(messageAvatar, messageAuthor, messageContent);
 
-  // ADD CLASS NAME TO EACH ELEMENT TO STYLE IT
+  // ADD CLASS NAME TO EACH ELEMENT (TO APPLY THE CSS STYLES)
   newMessage.classList = "message";
   messageAuthor.classList = "headline";
   messageAvatar.classList = "avatar";
@@ -62,20 +62,21 @@ function displayMessage(message, sender) {
     newMessage.classList += " align-right";
   }
 
-  // APPEND THE DIV WITH NEW ELEMENTS AND CONTENT TO THE CHAT
+  // APPEND THE DIV WITH NEW ELEMENTS TO THE CHAT ELEMENT
   chat.appendChild(newMessage);
 
-  // TRIGGER THE AUTO SCROLL TO BOTTOM
+  // UPDATE THE SCROLL POSITION OF THE CHAT, TO THE BOTTOM
   chat.scrollTop = chat.scrollHeight;
 
+  // IF SENDER WAS THE USER, CALL THE `replyBack` FUNCTION AFTER 2 SECONDS
   if (sender === "Me") {
-    newMessage.classList += " align-right";
     setTimeout(replyBack, 2000);
   }
 }
 
-// ADD EVENT LISTENERS
+// EVENT LISTENERS
 sendButton.addEventListener("click", sendMessage);
+
 messageInput.addEventListener("keypress", function(event) {
   if (event.key === "Enter") sendMessage(event);
 });
